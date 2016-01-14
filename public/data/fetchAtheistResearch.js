1e6141401306f1a185d2af91f14612de63513dfa
Paul Ericksen <liquidmonkey@gmail.com>
2016-01-14 14:18:21 -0700

    starting to switch to nodejs server

--- public/data/fetchAtheistResearch.js
+++ public/data/fetchAtheistResearch.js
@@ -1,7 +1,8 @@
-var http = require('http');
+var http = require('https');
 var fs = require('fs');
 
-var file = fs.createWriteStream("atheist_data.csv");
-var request = http.get("http://docs.google.com/spreadsheets/d/1KP7ULBAyavnohP4h8n2J2yaXNpIRnyIXdjJj_AwtwK0/export?gid=1&format=csv", function(response) {
+var file = fs.createWriteStream("./public/data/atheist_data.csv")
+var spread = 'https://docs.google.com/spreadsheets/d/1KP7ULBAyavnohP4h8n2J2yaXNpIRnyIXdjJj_AwtwK0/export?gid=1&format=csv'
+var request = http.get(spread, function(response) {
     response.pipe(file);
 });
