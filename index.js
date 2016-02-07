b07671a920e64cc81d33524105cd46bf76149483
Paul Ericksen <liquidmonkey@gmail.com>
2016-02-06 20:56:05 -0700

    removed ejs view engine, made index a static page.

--- index.js
+++ index.js
@@ -71,12 +71,8 @@ app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'))
 
 // views is directory for all template files
-app.set('views', __dirname + '/views')
-app.set('view engine', 'ejs')
-
-app.get('/', function(request, response) {
-  response.render('pages/index')
-})
+//app.set('views', __dirname + '/views')
+//app.set('view engine', 'ejs')
 
 //old site bad urls.
 app.get('/feed.svc/rss/*', function(request, response){
