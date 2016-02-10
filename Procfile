922ebd9e75ae946be0591aa8286949ff01715ece
Paul Ericksen <liquidmonkey@gmail.com>
2016-02-10 16:52:46 -0700

    trying clustering again

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node server.js
+web: node cluster.js
--- cluster.js
+++ cluster.js
@@ -1,50 +1,31 @@
 "use strict";
 
-/**
- * NOT USED. Failed experiment. Caused too much memory usage.
- */
-
+var Hub = require('cluster-hub')
+var hub = new Hub();
 const cluster = require('cluster')
-const k = require('./react/src/scripts/api/kiva')
-const KLPageSplits = k.KLPageSplits
-var axon = require('axon');
-require('./react/src/scripts/linqextras')
-//to satisfy kiva.js
-global.cl = function(){}
+var memwatch = require('memwatch-next')
+var extend = require('extend')
+const util = require('util')
 
-//session stuff (unused at this point)
-//app.set('trust proxy', 1) // trust first proxy
-/** app.use( session({
-        secret : 'k15yWt1w2k5M45Wrb1V02PzBqXuBjUsN', //switch to heroku config once I use this in prod.
-        name : 'sessionId',
-    })
- )**/
-
-const axonPort = 3005
+function outputMemUsage(event){
+    console.log(event, util.inspect(process.memoryUsage()), `uptime: ${process.uptime()}`)
+}
 
 function notifyAllWorkers(msg){
     Object.keys(cluster.workers).forEach(id => cluster.workers[id].send(msg))
 }
 
-if (cluster.isMaster) {
-    const numCPUs = require('os').cpus().length
-    console.log("*** CPUs: " + numCPUs)
+const blankResponse = {loanChunks:'', newestTime: null, descriptions:''}
+var partnersGzip
+var loansToServe = {0: extend({},blankResponse)} //start empty.
+var latest = 0
 
-    //var loanChunks = []
+if (cluster.isMaster){ //preps the downloads
+    console.log("STARTING MASTER")
+    var zlib = require('zlib')
+    const gzipOpt = {level : zlib.Z_BEST_COMPRESSION}
 
-    Array.range(1,Math.min(numCPUs,1)).forEach(x=>cluster.fork())
-
-    var sock = axon.socket('rep')
-    sock.connect(axonPort)
-
-    //not used
-    sock.on('message', function(msg, reply){
-        console.log("sock.on('message'): ", msg)
-        if (msg.getStart)
-            reply({pages: loanChunks.length})
-        if (msg.getPage)
-            reply(loanChunks[msg.getPage - 1])
-    })
+    cluster.fork()
 
     // Listen for dying workers
     cluster.on('exit', function (worker) {
@@ -54,101 +35,178 @@ if (cluster.isMaster) {
         cluster.fork()
     })
 
+    hub.on("filter", (newestTime, sender, callback) => {
+        var loans = kivaloans.loans_from_kiva.where(l=>l.kl_processed.getTime() > newestTime)
+        if (loans.length > 500) {
+            //todo: make a better way to find changes than kl_processed since that gets reset on background resync
+            console.log(`INTERESTING: loans/since count: ${loans.length}: NOT SENDING`)
+            cb(JSON.stringify([]))
+            return
+        }
+        console.log(`INTERESTING: loans/since count: ${loans.length}`)
+        callback(JSON.stringify(k.ResultProcessors.unprocessLoans(loans)))
+    })
+
+    var k = require('./react/src/scripts/api/kiva')
+    const KLPageSplits = k.KLPageSplits
+    k.setAPIOptions({max_concurrent:20})
+
+    //to satisfy kiva.js ; hack
+    global.cl = function(){}
+
+    require('./react/src/scripts/linqextras')
+
     /**
      * issues: partners don't get updated after initial load.
      * so it just reinitializes after 24 hours of constant running
+     * it should re-download partners and atheist list on some schedule
+     * in the client as well for long-running clients (some are open for weeks!)
      */
 
     var kivaloans
     var loansChanged = false
 
-    //todo: not used
-    cluster.on('message',(msg,handle,callback)=>{
-        console.log("cluster.on(message):", msg,handle,callback)
-        if (msg.getStart)
-            callback({pages: loanChunks.length})
-        if (msg.getPage)
-            callback(loanChunks[msg.getPage - 1])
+    //
+    //temporary fix for memory issue. the restart is so fast and the client is usable before KL
+    //has all loans loaded... have this not do it every 24 hours, but on an interval check the time
+    //or when it starts up have it calculate when midnight is and set a timeout.
+    setInterval(()=>{
+        console.log('INTERESTING: restart on interval')
+        process.exit(1)
+    }, 24*60*60000)
+
+
+    kivaloans = new k.Loans(5*60*1000)
+    var getOptions = ()=>({loansFromKL:false,loansFromKiva:true,mergeAtheistList:true})
+    kivaloans.init(null, getOptions, {app_id: 'org.kiva.kivalens', max_concurrent: 8}).progress(progress => {
+        if (progress.loan_load_progress && progress.loan_load_progress.label)
+            console.log(progress.loan_load_progress.label)
+        if (progress.loans_loaded || progress.background_added || progress.background_updated || progress.loan_updated || progress.loan_not_fundraising || progress.new_loans)
+            loansChanged = true
+        if (progress.loans_loaded || (progress.backgroundResync && progress.backgroundResync.state == 'done'))
+            prepForRequests()
     })
 
-    const tempFixReInitKivaLoans = function(){
-        kivaloans = new k.Loans(5*60*1000)
-        var getOptions = ()=>({loansFromKL:false,loansFromKiva:true,mergeAtheistList:true}) //todo:second is not implemented yet.
-        kivaloans.init(null, getOptions, {app_id: 'org.kiva.kivalens', max_concurrent: 8}).progress(progress => {
-            if (progress.loan_load_progress && progress.loan_load_progress.label)
-                console.log(progress.loan_load_progress.label)
-            if (progress.loans_loaded || progress.background_added || progress.background_updated || progress.loan_updated || progress.loan_not_fundraising || progress.new_loans)
-                loansChanged = true
-            if (progress.loans_loaded)
-                prepForRequests()
-        })
-    }
-
-    setInterval(tempFixReInitKivaLoans, 24*60*60000)
-    tempFixReInitKivaLoans()
-
     const prepForRequests = function(){
-        if (!kivaloans.isReady()){
+        if (!kivaloans.isReady()) {
             console.log("kivaloans not ready")
             return
         }
         if (!loansChanged) {
-            //console.log("Nothing changed")
+            console.log("Nothing changed")
             return
         }
+
+        outputMemUsage('prepForRequests')
+
         loansChanged = false //hot loans &
-        kivaloans.loans_from_kiva.removeAll(l=>l.status!='fundraising')
+        var prepping = extend({}, blankResponse)
+
+        kivaloans.loans_from_kiva.removeAll(l=>l.status != 'fundraising')
         var allLoans = k.ResultProcessors.unprocessLoans(kivaloans.loans_from_kiva)
+        //additional unprocessing and collecting descriptions
+        var descriptions = []
+        allLoans.forEach(loan => {
+            descriptions.push({id: loan.id, t: loan.kls_use_or_descr_arr}) //only need to do descr... use already there.
+            delete loan.description //.texts.en
+            delete loan.kls_use_or_descr_arr
+            if (!loan.kls_age) delete loan.kls_age
+            delete loan.lender_count
+            if (!loan.funded_amount) delete loan.funded_amount
+            if (!loan.basket_amount) delete loan.basket_amount
+            if (!loan.kls_tags.length) delete loan.kls_tags
+            delete loan.terms.repayment_term
+            loan.klb = {}
+            loan.borrowers.groupByWithCount(b=>b.gender).forEach(g=>loan.klb[g.name] = g.count)
+            delete loan.borrowers
+            delete loan.terms.loss_liability.currency_exchange_coverage_rate
+            delete loan.borrower_count
+            delete loan.payments
+            delete loan.status
+            loan.kls = true
+        })
         var chunkSize = Math.ceil(allLoans.length / KLPageSplits)
-        notifyAllWorkers({loanChunks: allLoans.chunk(chunkSize)}) //.select(chunk => JSON.stringify(chunk))})
-        //todo: loanChunks = allLoans.chunk(chunkSize).select(chunk => JSON.stringify(chunk))
-        console.log("Loan chunks ready!")
+        prepping.newestTime = kivaloans.loans_from_kiva.max(l=>l.kl_processed.getTime())
+        var bigloanChunks = allLoans.chunk(chunkSize).select(chunk => JSON.stringify(chunk))
+        prepping.loanChunks = Array.range(0, KLPageSplits).select(x=>'')
+        var bigDesc = descriptions.chunk(chunkSize).select(chunk => JSON.stringify(chunk))
+        prepping.descriptions = Array.range(0, KLPageSplits).select(x=>'')
+
+        function finishIfReady(){
+            if (prepping.loanChunks.all(c=>c != '') && prepping.descriptions.all(c=>c != '') && partnersGzip) {
+                loansToServe[++latest] = prepping //must make a copy.
+                //delete the old batches.
+                Object.keys(loansToServe).where(key => key < latest - 1).forEach(key => delete loansToServe[key])
+                console.log(`Loan chunks ready! Chunks: ${prepping.loanChunks.length} Batch: ${latest} Cached: ${Object.keys(loansToServe).length}`)
+                bigloanChunks = undefined
+                bigDesc = undefined
+                notifyAllWorkers({downloadReady: JSON.stringify({latest, loansToServe, partnersGzip})})
+                outputMemUsage('finishIfReady:before GC')
+                memwatch.gc()
+                outputMemUsage('finishIfReady:after GC')
+            }
+        }
+
+        zlib.gzip(JSON.stringify(kivaloans.partners_from_kiva), gzipOpt, function (_, result) {
+            partnersGzip = result
+            finishIfReady()
+        })
+
+        bigloanChunks.map((chunk, i) => { //map to give index
+            zlib.gzip(chunk, gzipOpt, function (_, result) {
+                prepping.loanChunks[i] = result
+                finishIfReady()
+            })
+        })
+
+        bigDesc.map((chunk, i) => {
+            zlib.gzip(chunk, gzipOpt, function (_, result) {
+                prepping.descriptions[i] = result
+                finishIfReady()
+            })
+        })
     }
 
-    //prep it every 30 seconds
-    setInterval(prepForRequests, 30000)
+    setInterval(prepForRequests, 60000)
 
+    //live data stream over socket.io
     const connectChannel = function(channelName, onEvent) {
         var channel = require('socket.io-client').connect(`http://streams.kiva.org:80/${channelName}`,{'transports': ['websocket']});
-        //channel.on('connect', function () {console.log(`socket.io channel connect: ${channelName}`)})
         channel.on('error', function (data) {console.log(`socket.io channel error: ${channelName}: ${data}`)})
-        channel.on('event', onEvent)
         channel.on('message', onEvent)
-        //channel.on('disconnect', function () {console.log(`socket.io channel disconnect: ${channelName}`)})
     }
 
     connectChannel('loan.posted', function(data){
         data = JSON.parse(data)
         console.log("!!! loan.posted")
-        loansChanged = true
         if (kivaloans)
             kivaloans.queueNewLoanNotice(data.p.loan.id)
     })
 
     connectChannel('loan.purchased', function(data){
         data = JSON.parse(data)
-        console.log("!!! loan.purchased")
-        loansChanged = true
+        var ids = data.p.loans.select(l=>l.id)
+        console.log("!!! loan.purchased: " + ids.length)
         if (kivaloans)
-            kivaloans.queueToRefresh(data.p.loans.select(l=>l.id))
+            kivaloans.queueToRefresh(ids)
     })
-
-} else {
+}
+else
+{ //workers handle the downloads
+    console.log("STARTING WORKER")
     var express = require('express')
     var app = express()
     var proxy = require('express-http-proxy')
     var helmet = require('helmet')
-    //var session = require('express-session')
-
-    var sock = axon.socket('req')
-    sock.bind(axonPort)
-
     var compression = require('compression')
+
     // compress all requests
     app.use(compression())
+
     //some security
     app.use(helmet())
-    var loanChunks = []
+
+    //TODO: RESTRICT TO SAME SERVER?
     const proxyHandler = {
         forwardPath: function(req, res) {
             return require('url').parse(req.url).path;
@@ -166,6 +224,7 @@ if (cluster.isMaster) {
             }
         }
     }
+
     //PASSTHROUGH
     app.use('/proxy/kiva', proxy('https://www.kiva.org', proxyHandler))
     app.use('/proxy/gdocs', proxy('https://docs.google.com', proxyHandler))
@@ -174,69 +233,106 @@ if (cluster.isMaster) {
 
     app.use(express.static(__dirname + '/public'))
 
-    // views is directory for all template files
-    app.set('views', __dirname + '/views')
-    app.set('view engine', 'ejs')
-
-    app.get('/', function(request, response) {
-        response.render('pages/index')
-    })
-
     //old site bad urls.
     app.get('/feed.svc/rss/*', function(request, response){
         response.sendStatus(404)
     })
 
     //API
-    app.get('/loans/start', function(request, response){
-        var data = {pages: loanChunks.length}
-        response.send(JSON.stringify(data))
-        /**todo:sock.send({getStart:true}, result => {
-            response.send(JSON.stringify(result))
-        })**/
+    app.get('/start', function(request, response){
+        response.json({pages: loansToServe[latest].loanChunks.length, batch: latest})
     })
 
-    app.get('/loans/get', function(request, response) {
-        var page = parseInt(request.param('page'))
-        if (page) {
-            if (page > KLPageSplits || page < 1) {
-                response.sendStatus(404)
-                return
-            }
-            response.send(JSON.stringify(loanChunks[page - 1]))
-            /**sock.send({getPage:page}, result => {
-                response.send(result)
-            })**/
-        } else {
+    app.get('/loans/:batch/:page', function(request, response) {
+        var batch = parseInt(request.params.batch)
+        if (batch != latest)
+            console.log(`INTERESTING: /loans batch: ${batch} latest: ${latest}`)
+
+        if (!loansToServe[batch]) {
             response.sendStatus(404)
+            return
+        }
+        var page = parseInt(request.params.page)
+        var toServe = loansToServe[batch].loanChunks[page - 1]
+        if (!toServe) {
+            response.sendStatus(404)
+        } else {
+            response.header('Content-Type', 'application/json')
+            response.header('Content-Encoding', 'gzip')
+            response.send(toServe)
         }
     })
 
-    //this won't work anymore. kivaloans doesn't exist in the worker!
-    app.get('/loans/filter', function(req, resp){
-        //getUrl("http://www.kivalens.org/loans/filter?crit=" + encodeURIComponent(JSON.stringify({loan:{name:"Paul"}})),true).done(r => console.log(r))
-        var crit = req.param("crit")
-        if (crit)
-            crit = JSON.parse(decodeURIComponent(crit))
-        //var results = k.ResultProcessors.unprocessLoans(kivaloans.filter(crit, false))
-        var results = kivaloans.filter(crit, false).select(l=>l.id)
-        resp.send(JSON.stringify(results))
+    app.get('/partners', function(request,response){
+        //don't use 'batch' since it just serves all at once and we want the most recent.
+        response.header('Content-Type', 'application/json')
+        response.header('Content-Encoding', 'gzip')
+        response.send(partnersGzip)
     })
 
-    //CATCH ALL
-    //any page not defined in this file gets routed to everything which redirects to /#/search
+    app.get('/loans/:batch/descriptions/:page', function(request,response){
+        var batch = parseInt(request.params.batch)
+        if (!loansToServe[batch]) {
+            response.sendStatus(404)
+            return
+        }
+        if (batch != latest)
+            console.log(`INTERESTING: /loans/descriptions batch: ${batch} latest: ${latest}`)
+        var page = parseInt(request.params.page)
+        var toServe = loansToServe[batch].descriptions[page - 1]
+        if (!toServe) {
+            response.sendStatus(404)
+        } else {
+            response.header('Content-Type', 'application/json');
+            response.header('Content-Encoding', 'gzip');
+            response.send(toServe)
+        }
+    })
+
+    app.get('/since/:batch', function(request, response){
+        var batch = parseInt(request.params.batch)
+        if (!batch || !loansToServe[batch]){
+            response.sendStatus(404)
+            return
+        }
+        var newestTime = loansToServe[batch].newestTime
+        hub.requestMaster('filter', newestTime, result => response.send(result))
+    })
+
+    //req.kl.get("loans/filter", {crit: encodeURIComponent(JSON.stringify({loan:{name:"Paul"}}))},true).done(r => console.log(r))
+    app.get('/loans/filter', function(req, resp){
+        var crit = req.query.crit
+        if (crit)
+            crit = JSON.parse(decodeURIComponent(crit))
+        resp.json(kivaloans.filter(crit).select(l=>l.id))
+    })
+
+    //CATCH ALL this will also redirect old image reqs to a page though...
     app.get('/*', function(request, response) {
-        response.render('pages/everything') //can i do permanent redirect?
+        response.redirect("/#/search")
     })
 
     app.listen(app.get('port'), function() {
         console.log('KivaLens Server is running on port', app.get('port'))
     })
 
+    //worker receiving message...
     process.on("message", msg => {
-        if (msg.loanChunks){
-            loanChunks = msg.loanChunks
-            console.log("received loanChunks message")
+        if (msg.downloadReady){
+            outputMemUsage('WORKER NEW STUFF')
+            const dl = JSON.parse(msg.downloadReady, (key, value) => {
+                return value && value.type === 'Buffer'
+                    ? new Buffer(value.data)
+                    : value;
+            })
+            loansToServe = dl.loansToServe
+            latest = dl.latest
+            partnersGzip = dl.partnersGzip
+            memwatch.gc()
+            outputMemUsage('WORKER NEW STUFF: after GC')
+            console.log("received downloadReady message")
         }
     })
 }
+
+
