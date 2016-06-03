c709ec766acca9230399950babe1b3331c748509
Paul Ericksen <liquidmonkey@gmail.com>
2016-06-02 21:41:38 -0600

    adding cross-origin access to graphql api for KLA

--- cluster.js
+++ cluster.js
@@ -605,9 +605,22 @@ else  //workers handle all communication with the clients.
     var graphqlHTTP = require('express-graphql');
     var schema = require('./schema');
     var main = express()
-    
+
+    var allowCrossDomain = function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+        // intercept OPTIONS method
+        if ('OPTIONS' == req.method) {
+            res.send(200);
+        } else {
+            next();
+        }
+    };
 
     // compress all requests
+    app.use(allowCrossDomain)
     app.use(compression())
 
     app.use('/graphql', graphqlHTTP({ schema: schema, graphiql: true, pretty: true }));
